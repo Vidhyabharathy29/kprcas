0422ecd810a5d89df06c0d2bf8c52f99128089be
Vidhyabharathy29 <Vidhyabharathy2006@gmail.com>
2025-06-06 16:13:04 +0530

    first command

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log(5==='5')
+let i=0
+do{
+    i++;
+    console.log(i);
+}while(i<5)
+function greet(name ,age){
+    return`hello ${name} is ${age} old`
+}
+console.log(greet('Ronaldo',40));
+function  add(a,b){
+    return'The sum of '+a +' and ' +b+ ' is '+a+b
+}
+console.log(add(3,6))
